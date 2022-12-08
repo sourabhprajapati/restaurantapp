ee825d684de2f0c71766bf99ccf9ec1aabc858fd
sourabhprajapati <sourabhprajapati920@gmail.com>
2022-12-08 20:48:41 +0530

    update

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FooterOverlay,Newsletter } from '../../components'; 
+import './Footer.css';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { images } from '../../constants';
+const Footer = () => (
+  <div className='app__footer section__padding'>
+    <FooterOverlay/>
+    <Newsletter/>
+    <div className='app__footer-links'>
+      <div className='app__footer-links_contact'>
+      <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">A-8B janta colony Raja Park,Jaipur</p>
+        <p className="p__opensans">+91-8888999222</p>
+        <p className="p__opensans">+91-8888999111</p>
+
+      </div>
+      <div className='app__footer-links_logo'>
+        <img src={images.gericht} alt="footer_logo"/>
+        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+
+      </div>
+      <div className='app__footer-links_work'>
+      <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+        
+      </div>
+     
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from "../../constants";
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+        <SubHeading title="Chase the new flavour"/>
+        <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+        <p className='p__opensans' style={{ margin:'2rem 0'}}>
+        Lorem ipsum dolor sit amet. Sit praesentium distinctio qui libero libero et optio earum. Ea voluptate suscipit eum ipsam nisi est totam unde quo optio neque! 
+        </p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+        <img src={images.welcome} alt="header img"/>
+    </div>
+         
+  </div>
+);
+
+export default Header;
